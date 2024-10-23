5bc1975a0ee2bb11c9a96e9843193724e4d38c16
Ari Wiradana <ariwiradana5@gmail.com>
2024-10-23 21:00:30 +0800

    modified:   components/themes/Flora/layouts/participants.tsx

--- components/themes/Flora/layouts/participants.tsx
+++ components/themes/Flora/layouts/participants.tsx
@@ -1,7 +1,6 @@
 import React, { FC } from "react";
-import { engagement, marcellus } from "@/lib/fonts";
+import {  italiana, marcellus } from "@/lib/fonts";
 import ImageShimmer from "../../../image.shimmer";
-import Title from "../elements/title";
 import { useSamaya } from "@/hooks/themes/useSamaya";
 import { Participant } from "@/lib/types";
 import Link from "next/link";
@@ -11,7 +10,6 @@ import {
   BiLogoTiktok,
   BiLogoTwitter,
 } from "react-icons/bi";
-import Image from "next/image";
 
 interface Props {
   state: useSamaya["state"];
@@ -23,28 +21,9 @@ const ParticipantsComponent: FC<Props> = (props) => {
     props.state.client?.participants.length > 0
   )
     return (
-      <section className="relative bg-samaya-dark z-10 pb-4 pt-16 md:py-16">
-        <div className="w-full h-full px-4 md:px-12 relative z-40 max-w-screen-xl mx-auto">
-          <div className="mb-8" data-aos="fade-up">
-            <Title title={props.state.client?.opening_title as string} />
-          </div>
-          <p
-            data-aos="fade-up"
-            data-aos-delay="100"
-            className={`${marcellus.className} text-base md:text-lg text-center leading-5 text-white mb-12 max-w-screen-md mx-auto`}
-          >
-            {props.state.client?.opening_description}
-          </p>
-
-          <div className="grid md:grid-cols-2 w-full gap-20 py-8">
-            {props.state.groom && (
-              <ParticipantComponent data={props.state?.groom as Participant} />
-            )}
-            {props.state.bride && (
-              <ParticipantComponent data={props.state?.bride as Participant} />
-            )}
-          </div>
-        </div>
+      <section className="relative bg-white z-10">
+        {props.state.groom && <ParticipantComponent data={props.state.groom} />}
+        {props.state.bride && <ParticipantComponent data={props.state.bride} />}
       </section>
     );
 };
@@ -54,60 +33,51 @@ interface ComponentProps {
 }
 const ParticipantComponent: FC<ComponentProps> = (props) => {
   return (
-    <div className="flex flex-col justify-center items-center">
+    <div
+      className={`flex flex-col py-12 px-8 ${
+        props.data.role === "groom"
+          ? "bg-flora-primary/40"
+          : "bg-flora-primary/60"
+      }`}
+    >
       {props.data.image && (
-        <div data-aos="zoom-in" className="relative w-[277px] h-[368px]">
+        <div className="relative w-full aspect-[3/4] shadow-lg self-center">
           <ImageShimmer
             fill
             priority
-            sizes="277px"
-            className="object-cover w-full h-full overflow-hidden p-[10px]"
+            sizes="311px"
+            className="object-cover w-full h-full"
             src={props.data.image as string}
             alt={props.data.name}
           />
-          <div className="absolute inset-0 bg-black bg-opacity-20"></div>
-          <Image
-            className="absolute inset-0 z-30"
-            alt="floral-top-corner"
-            src="/images/samaya/frame-primary.svg"
-            height={277}
-            width={368}
-          />
         </div>
       )}
+      <p
+        className={`${marcellus.className} uppercase mt-9 text-sm text-flora-dark/40 border-b border-b-flora-dark/20 pb-1`}
+      >
+        The Groom
+      </p>
       <h1
-        data-aos="fade-up"
-        className={`text-4xl font-semibold text-samaya-primary relative mt-10 ${engagement.className}`}
+        className={`text-[28px] text-flora-dark relative mt-3 ${italiana.className}`}
       >
         {props.data.name}
       </h1>
-      <div className={`${marcellus.className} text-center`} data-aos="fade-up">
+      <div
+        className={`${marcellus.className} mt-4 mb-6 border-b border-b-flora-dark/20 pb-6`}
+      >
         {props.data.role !== "participant" && (
           <>
-            <p className="text-gray-300 text-sm md:text-lg mt-4 capitalize font-italic">
+            <p className="text-xs text-flora-dark/40 mb-1">
               {props.data.gender === "female" ? "Putri" : "Putra"}{" "}
               {props.data.child} dari pasangan
             </p>
-            <h2 className="text-base md:text-lg text-white mt-2">
+            <h2 className="text-sm text-flora-dark">
               Bapak {props.data.parents_male} & Ibu {props.data.parents_female}
             </h2>
           </>
         )}
       </div>
-      <div
-        className="w-[1px] h-7 bg-samaya-primary my-3"
-        data-aos="fade-up"
-      ></div>
-      <p
-        className={`text-samaya-primary text-base md:text-lg text-center ${marcellus.className}`}
-        data-aos="fade-up"
-      >
-        {props.data.address}
-      </p>
-      <div
-        className="flex mt-4 gap-x-2 text-samaya-primary text-2xl md:text-3xl text-center"
-        data-aos="fade-up"
-      >
+      <div className="flex gap-x-2 text-flora-dark text-lg">
         {props.data.facebook && (
           <Link
             aria-label="sosmed-facebook-link"
