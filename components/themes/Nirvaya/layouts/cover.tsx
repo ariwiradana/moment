67c2c31353513b84f3375934c653b16ac8aab51f
Ari Wiradana <ariwiradana5@gmail.com>
2024-10-30 03:08:42 +0800

    modified:   components/themes/Nirvaya/layouts/cover.tsx modified:   designs/Theme Card.psd modified:   designs/Themes/Nirvaya/Nirvaya - Cover.png modified:   designs/Themes/Nirvaya/Phone Mockup.png modified:   designs/Themes/Nirvaya/Theme Card.png

--- components/themes/Nirvaya/layouts/cover.tsx
+++ components/themes/Nirvaya/layouts/cover.tsx
@@ -2,7 +2,6 @@ import React, { FC } from "react";
 import { balthazar, italiana, marcellus } from "@/lib/fonts";
 import Button from "../elements/button";
 import { useNirvaya } from "@/hooks/themes/useNirvaya";
-import Image from "next/image";
 import { BiSolidEnvelopeOpen } from "react-icons/bi";
 
 interface Props {
@@ -15,28 +14,16 @@ const Cover: FC<Props> = (props) => {
   return (
     <>
       <div
-        className={`w-full h-dvh fixed inset-x-0 transition-all ease-in-out duration-1000 bg-gradient-to-b from-nirvaya-dark/70 to-nirvaya-dark z-50 ${
-          props.state.open ? "-top-full invisible opacity-0" : "top-0 visible"
+        className={`w-full h-dvh fixed inset-x-0 transition-all ease-in-out duration-1000 z-50 ${
+          props.state.open
+            ? "-bottom-full invisible opacity-0"
+            : "bottom-0 visible"
         }`}
       >
-        <div className="flex h-dvh flex-col items-center justify-center relative z-30">
-          {props.state.client?.cover && (
-            <div
-              className="relative w-[134px] h-[171px] md:w-[154px] md:h-[191px] rounded-t-[100px] overflow-hidden rounded-b-[20px]"
-              data-aos="zoom-in-up"
-              data-aos-delay="200"
-            >
-              <Image
-                fill
-                alt={`cover-img-image`}
-                priority
-                sizes="280px"
-                quality={90}
-                className="object-cover"
-                src={props.state.client?.cover as string}
-              />
-            </div>
-          )}
+        <div
+          data-aos="zoom-out-up"
+          className="flex h-dvh flex-col items-center justify-end relative z-30 py-[60px] md:py-[100px] px-8 bg-gradient-to-b from-transparent via-nirvaya-dark/50 via-[50%] to-[70%] to-nirvaya-dark"
+        >
           <p
             data-aos="fade-up"
             data-aos-delay="400"
@@ -47,12 +34,14 @@ const Cover: FC<Props> = (props) => {
           <h1
             data-aos="fade-up"
             data-aos-delay="600"
-            className={`${italiana.className} text-white text-[40px] md:text-5xl`}
+            className={`${italiana.className} text-white text-center text-[40px] md:text-5xl`}
           >
             {props.state.groom?.nickname} & {props.state.bride?.nickname}
           </h1>
           <div data-aos="fade-up" data-aos-delay="800">
-            <p className={`${balthazar.className} text-white text-xs md:text-sm mt-4 md:mt-6`}>
+            <p
+              className={`${balthazar.className} text-white text-xs md:text-sm mt-4 md:mt-6 text-center`}
+            >
               Yth. Bapak/Ibu/Saudara/i
             </p>
             <p
@@ -69,7 +58,11 @@ const Cover: FC<Props> = (props) => {
             Tanpa mengurangi rasa hormat, kami mengundang anda untuk menghadiri
             acara pernikahan kami
           </p>
-          <div className="mt-6 md:mt-8" data-aos="fade-up" data-aos-delay="1200">
+          <div
+            className="mt-6 md:mt-8"
+            data-aos="fade-up"
+            data-aos-delay="1200"
+          >
             <Button
               onClick={() => {
                 props.actions.handleOpenCover();
