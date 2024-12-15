cfb753627ed429260887c3fe0f193e458d2cafea
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-15 10:51:46 +0800

    modified:   lib/db.ts

--- lib/db.ts
+++ lib/db.ts
@@ -3,4 +3,9 @@ import { Pool } from "pg";
 const sql = new Pool({
   connectionString: process.env.POSTGRES_URL,
 });
+
+sql.on("error", (err) => {
+  console.error("SQL error:", err);
+});
+
 export default sql;
