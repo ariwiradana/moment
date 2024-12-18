98c0051b94ec4fdf9103de433ef431782c0ebeec
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-18 17:06:58 +0800

    update

--- components/themes/themes.tsx
+++ components/themes/themes.tsx
@@ -1,8 +1,6 @@
 import { Client } from "@/lib/types";
 import React from "react";
 import Samaya from "./Samaya/theme";
-import Aakarshana from "./Akarshana/theme";
-import Flora from "./Flora/theme";
 import Nirvaya from "./Nirvaya/theme";
 import Aruna from "./Aruna/theme";
 
@@ -10,14 +8,8 @@ export const themes: Record<
   string,
   (client: Client, untuk: string) => JSX.Element
 > = {
-  Aakarshana: (client: Client, untuk: string) => (
-    <Aakarshana client={client} untuk={untuk} />
-  ),
   Samaya: (client: Client, untuk: string) => (
     <Samaya client={client} untuk={untuk} />
-  ),
-  Flora: (client: Client, untuk: string) => (
-    <Flora client={client} untuk={untuk} />
   ),
   Nirvaya: (client: Client, untuk: string) => (
     <Nirvaya client={client} untuk={untuk} />
