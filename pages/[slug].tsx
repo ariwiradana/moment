ddbdb9e8acf52da5753d71ee8c2c9d6fece53249
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-19 00:55:21 +0800

    modified:   pages/[slug].tsx

--- pages/[slug].tsx
+++ pages/[slug].tsx
@@ -33,6 +33,8 @@ const MainPage: FC<Props> = ({ untuk, client: clientData }) => {
     });
   }, []);
 
+  if (!clientData) return <ClientNotFound />;
+
   const themeName = client?.theme?.name || "";
   const ThemeComponent = themes[themeName];
 
@@ -53,22 +55,19 @@ const MainPage: FC<Props> = ({ untuk, client: clientData }) => {
 
   return (
     <>
-      {client && (
-        <Seo
-          url={`https://momentinvitations.com/${client?.slug || ""}`}
-          title={pageTitle}
-          description={`${client?.opening_title || ""}, ${
-            client?.opening_description || ""
-          }`}
-          keywords="undangan digital, undangan online, undangan pernikahan, undangan metatah, undangan digital bali, undangan bali, undangan digital, platform undangan online, Moment Invitation, template undangan digital, undangan pernikahan digital, undangan online, undangan digital dengan RSVP, undangan dengan Google Maps, undangan digital premium, buat undangan digital, undangan digital minimalis, momentinvitations"
-          image={
-            client?.seo ||
-            "https://res.cloudinary.com/dwitznret/image/upload/v1734241503/seo_xftrjs.webp"
-          }
-        />
-      )}
+      <Seo
+        url={`https://momentinvitations.com/${client?.slug || ""}`}
+        title={pageTitle}
+        description={`${client?.opening_title || ""}, ${
+          client?.opening_description || ""
+        }`}
+        keywords="undangan digital, undangan online, undangan pernikahan, undangan metatah, undangan digital bali, undangan bali, undangan digital, platform undangan online, Moment Invitation, template undangan digital, undangan pernikahan digital, undangan online, undangan digital dengan RSVP, undangan dengan Google Maps, undangan digital premium, buat undangan digital, undangan digital minimalis, momentinvitations"
+        image={
+          client?.seo ||
+          "https://res.cloudinary.com/dwitznret/image/upload/v1734241503/seo_xftrjs.webp"
+        }
+      />
       {client && ThemeComponent(untuk)}
-      {!client && <ClientNotFound />}
     </>
   );
 };
