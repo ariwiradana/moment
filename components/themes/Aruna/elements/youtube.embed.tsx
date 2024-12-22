7211941d9e4cddaab96a4489b68656fc6a2b7397
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-22 12:02:58 +0800

    modified:   components/themes/Aruna/elements/youtube.embed.tsx

--- components/themes/Aruna/elements/youtube.embed.tsx
+++ components/themes/Aruna/elements/youtube.embed.tsx
@@ -1,21 +1,73 @@
-import React, { memo } from "react";
+import React, { memo, useEffect, useRef } from "react";
 
 interface YouTubeEmbedProps {
   youtubeId: string | null;
 }
 const YoutubeEmbed = ({ youtubeId }: YouTubeEmbedProps) => {
-  if (youtubeId)
-    return (
-      <div className="relative w-full aspect-video overflow-hidden">
-        <iframe
-          className="absolute top-0 left-0 w-full h-full"
-          src={`https://www.youtube.com/embed/${youtubeId}?autoplay=1&mute=1&loop=1&playlist=${youtubeId}&controls=1&modestbranding=1&showinfo=0&rel=0&vq=hd1080`}
-          title={`video-${youtubeId}`}
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
+  const videoContainerRef = useRef<HTMLDivElement | null>(null);
+  const iframeRef = useRef<HTMLIFrameElement | null>(null);
+
+  const loadVideo = () => {
+    if (videoContainerRef.current && !iframeRef.current) {
+      const iframe = document.createElement("iframe");
+      iframe.setAttribute(
+        "src",
+        `https://www.youtube.com/embed/${youtubeId}?enablejsapi=1&playlist=${youtubeId}&loop=1&modestbranding=1&showinfo=0&rel=0&vq=hd1080&mute=1`
+      );
+      iframe.setAttribute("class", "absolute top-0 left-0 w-full h-full");
+      iframe.setAttribute("frameborder", "0");
+      iframe.setAttribute(
+        "allow",
+        "accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+      );
+      iframe.setAttribute("allowfullscreen", "");
+      iframe.setAttribute("loading", "lazy");
+      iframeRef.current = iframe;
+      videoContainerRef.current.appendChild(iframe);
+    }
+  };
+
+  const controlVideo = (action: "playVideo" | "pauseVideo") => {
+    if (iframeRef.current && iframeRef.current.contentWindow) {
+      iframeRef.current.contentWindow.postMessage(
+        JSON.stringify({ event: "command", func: action }),
+        "*"
+      );
+    }
+  };
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          if (entry.isIntersecting) {
+            loadVideo();
+            controlVideo("playVideo");
+          } else {
+            controlVideo("pauseVideo");
+          }
+        });
+      },
+      { rootMargin: "0px 0px 300px 0px" }
     );
+
+    if (videoContainerRef.current) {
+      observer.observe(videoContainerRef.current);
+    }
+
+    return () => {
+      if (videoContainerRef.current) {
+        observer.unobserve(videoContainerRef.current);
+      }
+    };
+  }, []);
+
+  return (
+    <div
+      className="relative w-full aspect-video overflow-hidden"
+      ref={videoContainerRef}
+    ></div>
+  );
 };
 
 export default memo(YoutubeEmbed);
