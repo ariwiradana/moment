eb621d6a59fe66a78687fd1a0f7d64b31f2b4fce
Ari Wiradana <ariwiradana5@gmail.com>
2024-12-28 03:26:20 +0800

    modified:   pages/admin/wishes/index.tsx

--- pages/admin/wishes/index.tsx
+++ pages/admin/wishes/index.tsx
@@ -26,155 +26,159 @@ const WishesDashboard: React.FC<WishesDashboardProps> = ({ token }) => {
       <div className={`w-full ${montserrat.className}`}>
         <h1 className="text-2xl font-bold mb-4">Wishes Dashboard</h1>
 
-        {state.isLoading ? (
-          <Loader />
-        ) : (
-          <>
-            {state.clientOptions.length > 0 && (
-              <div className="md:flex">
-                <InputSelect
-                  onChange={(e) =>
-                    actions.handleChangeClient(parseInt(e.target.value))
-                  }
-                  value={state.clientId ?? ""}
-                  inputSize="medium"
-                  label="Client"
-                  options={state.clientOptions}
-                />
-              </div>
-            )}
-            <div className="mt-4 lg:mt-8">
-              <div className="grid grid-cols-1 md:grid-cols-2 lg:hidden gap-4">
-                {state.wishes.map((review) => (
-                  <div key={review.id} className="border rounded-lg p-3">
-                    <div className="flex justify-between items-center pb-3 border-b">
-                      <div className="flex justify-between items-center gap-x-2 w-full">
-                        <div className="flex items-center gap-x-3">
-                          <div
-                            style={{
-                              backgroundColor: getRandomColors(),
-                            }}
-                            className="w-8 h-8 rounded-full flex justify-center items-center font-medium text-white text-sm p-1"
-                          >
-                            {getInitial(review.name)}
-                          </div>
-                          <div>
-                            <h1 className="text-gray-800 font-semibold text-sm whitespace-nowrap">
-                              {review.name}
-                            </h1>
-                            <p className="text-gray-500 font-medium text-xs">
-                              {review.attendant}
-                            </p>
-                          </div>
-                        </div>
-                        <ButtonActionDialog>
-                          <ButtonText
-                            type="button"
-                            onClick={() => actions.handleDelete(review.id)}
-                            size="small"
-                            title="Delete"
-                            icon={<BiTrash className="text-base" />}
-                          />
-                        </ButtonActionDialog>
-                      </div>
-                    </div>
-                    <div className="pt-3 flex flex-col gap-y-2">
-                      <div>
-                        <p className="text-gray-500 font-medium text-xs">
-                          Wishes
-                        </p>
-                        <p className="text-gray-800 font-semibold text-sm">
-                          {review.wishes}
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                ))}
-              </div>
+        <>
+          {state.clientOptions.length > 0 && (
+            <div className="md:flex">
+              <InputSelect
+                onChange={(e) =>
+                  actions.handleChangeClient(parseInt(e.target.value))
+                }
+                value={state.clientId ?? ""}
+                inputSize="medium"
+                label="Client"
+                options={state.clientOptions}
+              />
+            </div>
+          )}
 
-              {state.toalRows > 0 && (
-                <div className="border border-gray-200 rounded-xl hidden lg:block">
-                  <table className="table-auto overflow-x-auto w-full table">
-                    <thead>
-                      <tr>
-                        <td className="px-4 py-1 text-gray-600 font-medium text-sm bg-gray-100 rounded-tl-xl">
-                          User
-                        </td>
-                        <td className="px-4 py-1 text-gray-600 font-medium text-sm bg-gray-100">
-                          Wishes
-                        </td>
-                        <td className="px-4 py-1 text-gray-600 font-medium text-sm bg-gray-100 rounded-tr-xl">
-                          Actions
-                        </td>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {state.wishes.map((review, index) => (
-                        <tr
-                          key={review.id}
-                          className={`border-b ${
-                            state.wishes.length - 1 === index
-                              ? "border-b-transparent"
-                              : "border-b-gray-200"
-                          }`}
-                        >
-                          <td className="px-4 py-3 text-gray-800 font-semibold text-sm">
-                            <div className="flex items-center gap-x-3">
-                              <div
-                                style={{
-                                  backgroundColor: getRandomColors(),
-                                }}
-                                className="h-10 w-10 rounded-full aspect-square flex justify-center items-center text-base"
-                              >
-                                <span className="text-white font-medium">
-                                  {getInitial(review.name)}
-                                </span>
-                              </div>
-                              <div>
-                                <span className="whitespace-nowrap">
-                                  {review.name}
-                                </span>
-                                <p className="text-gray-500 font-medium text-xs">
-                                  {review.attendant}
-                                </p>
-                              </div>
+          {state.isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              <div className="mt-4 lg:mt-8">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:hidden gap-4">
+                  {state.wishes.map((review) => (
+                    <div key={review.id} className="border rounded-lg p-3">
+                      <div className="flex justify-between items-center pb-3 border-b">
+                        <div className="flex justify-between items-center gap-x-2 w-full">
+                          <div className="flex items-center gap-x-3">
+                            <div
+                              style={{
+                                backgroundColor: getRandomColors(),
+                              }}
+                              className="w-8 h-8 rounded-full flex justify-center items-center font-medium text-white text-sm p-1"
+                            >
+                              {getInitial(review.name)}
                             </div>
-                          </td>
-
-                          <td className="px-4 py-3 text-gray-800 text-sm">
+                            <div>
+                              <h1 className="text-gray-800 font-semibold text-sm whitespace-nowrap">
+                                {review.name}
+                              </h1>
+                              <p className="text-gray-500 font-medium text-xs">
+                                {review.attendant}
+                              </p>
+                            </div>
+                          </div>
+                          <ButtonActionDialog>
+                            <ButtonText
+                              type="button"
+                              onClick={() => actions.handleDelete(review.id)}
+                              size="small"
+                              title="Delete"
+                              icon={<BiTrash className="text-base" />}
+                            />
+                          </ButtonActionDialog>
+                        </div>
+                      </div>
+                      <div className="pt-3 flex flex-col gap-y-2">
+                        <div>
+                          <p className="text-gray-500 font-medium text-xs">
+                            Wishes
+                          </p>
+                          <p className="text-gray-800 font-semibold text-sm">
                             {review.wishes}
+                          </p>
+                        </div>
+                      </div>
+                    </div>
+                  ))}
+                </div>
+
+                {state.toalRows > 0 && (
+                  <div className="border border-gray-200 rounded-xl hidden lg:block">
+                    <table className="table-auto overflow-x-auto w-full table">
+                      <thead>
+                        <tr>
+                          <td className="px-4 py-1 text-gray-600 font-medium text-sm bg-gray-100 rounded-tl-xl">
+                            User
                           </td>
-                          <td className="px-4 py-3 text-gray-800 font-semibold text-sm">
-                            <ButtonActionDialog>
-                              <ButtonText
-                                type="button"
-                                onClick={() => actions.handleDelete(review.id)}
-                                size="medium"
-                                title="Delete"
-                                icon={<BiTrash className="text-base" />}
-                              />
-                            </ButtonActionDialog>
+                          <td className="px-4 py-1 text-gray-600 font-medium text-sm bg-gray-100">
+                            Wishes
+                          </td>
+                          <td className="px-4 py-1 text-gray-600 font-medium text-sm bg-gray-100 rounded-tr-xl">
+                            Actions
                           </td>
                         </tr>
-                      ))}
-                    </tbody>
-                  </table>
+                      </thead>
+                      <tbody>
+                        {state.wishes.map((review, index) => (
+                          <tr
+                            key={review.id}
+                            className={`border-b ${
+                              state.wishes.length - 1 === index
+                                ? "border-b-transparent"
+                                : "border-b-gray-200"
+                            }`}
+                          >
+                            <td className="px-4 py-3 text-gray-800 font-semibold text-sm">
+                              <div className="flex items-center gap-x-3">
+                                <div
+                                  style={{
+                                    backgroundColor: getRandomColors(),
+                                  }}
+                                  className="h-10 w-10 rounded-full aspect-square flex justify-center items-center text-base"
+                                >
+                                  <span className="text-white font-medium">
+                                    {getInitial(review.name)}
+                                  </span>
+                                </div>
+                                <div>
+                                  <span className="whitespace-nowrap">
+                                    {review.name}
+                                  </span>
+                                  <p className="text-gray-500 font-medium text-xs">
+                                    {review.attendant}
+                                  </p>
+                                </div>
+                              </div>
+                            </td>
+
+                            <td className="px-4 py-3 text-gray-800 text-sm">
+                              {review.wishes}
+                            </td>
+                            <td className="px-4 py-3 text-gray-800 font-semibold text-sm">
+                              <ButtonActionDialog>
+                                <ButtonText
+                                  type="button"
+                                  onClick={() =>
+                                    actions.handleDelete(review.id)
+                                  }
+                                  size="medium"
+                                  title="Delete"
+                                  icon={<BiTrash className="text-base" />}
+                                />
+                              </ButtonActionDialog>
+                            </td>
+                          </tr>
+                        ))}
+                      </tbody>
+                    </table>
+                  </div>
+                )}
+              </div>
+              {state.toalRows > state.limit && (
+                <div className="mt-6 flex justify-center">
+                  <Pagination
+                    page={state.page}
+                    onChange={actions.handleChangePagination}
+                    count={Math.ceil(state.toalRows / state.limit)}
+                    shape="rounded"
+                  />
                 </div>
               )}
-            </div>
-          </>
-        )}
-
-        {state.toalRows > state.limit && (
-          <div className="mt-6 flex justify-center">
-            <Pagination
-              page={state.page}
-              onChange={actions.handleChangePagination}
-              count={Math.ceil(state.toalRows / state.limit)}
-              shape="rounded"
-            />
-          </div>
-        )}
+            </>
+          )}
+        </>
       </div>
     </AdminLayout>
   );
