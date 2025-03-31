a0184ac83657f15313cfe489fb0be812875d5315
Ari Wiradana <ariwiradana5@gmail.com>
2025-03-31 19:34:48 +0800

    modified:   components/themes/Nirvaya/layouts/cover.tsx

--- components/themes/Nirvaya/layouts/cover.tsx
+++ components/themes/Nirvaya/layouts/cover.tsx
@@ -27,8 +27,8 @@ const Cover = ({ to, actions }: Props) => {
   return (
     <section
       data-aos="fade-in"
-      className={`fixed h-dvh transition-all ease-in-out duration-700 ${
-        isOpen ? "-bottom-full opacity-0" : "bottom-0 opacity-100"
+      className={`fixed transition-all ease-in-out duration-700 ${
+        isOpen ? "-bottom-full opacity-0 h-lvh" : "bottom-0 opacity-100 h-svh"
       } inset-x-0 bg-gradient-to-b from-nirvaya-dark/30 via-nirvaya-dark/0 to-nirvaya-dark/70 to-[70%] z-20 pb-16 ${
         raleway.className
       }`}
