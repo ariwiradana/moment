05f8731f9925c9df6b6ed266b114b4d3c008082c
Ari Wiradana <ariwiradana5@gmail.com>
2025-04-01 09:30:06 +0800

    ts

--- components/themes/Nirvaya/layouts/gift.tsx
+++ components/themes/Nirvaya/layouts/gift.tsx
@@ -4,8 +4,7 @@ import { formatBankNumber } from "@/utils/formatBankNumber";
 import { NextPage } from "next";
 import { BiGift, BiSolidCopy } from "react-icons/bi";
 
-
-const Gift: NextPage= () => {
+const Gift: NextPage = () => {
   const { actions } = useGift(<BiGift />);
   const { client } = useClientStore();
   return (
@@ -26,7 +25,7 @@ const Gift: NextPage= () => {
             hadiah kepada kedua mempelai dapat mengirimkannya melalui
           </p>
         </div>
-        <div data-aos="fade-up" className="mt-8 pr-8 md:pr-0">
+        <div data-aos="fade-up" className="mt-8 md:mt-16 pr-8 md:pr-0">
           <div className="bg-nirvaya-primary p-4 md:p-8">
             <h2 className="text-white w-full text-2xl md:text-3xl font-edensor whitespace-nowrap leading-8">
               {client?.gift_bank_name}
