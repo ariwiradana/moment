07bedc92bb9b70ba23eaf37a381c88544f1966eb
Ari Wiradana <ariwiradana5@gmail.com>
2025-04-11 08:23:38 +0800

    updated

--- components/themes/Luma/theme.tsx
+++ components/themes/Luma/theme.tsx
@@ -12,6 +12,9 @@ import Image from "next/image";
 import usePhotos from "@/hooks/themes/usePhotos";
 import Photos from "./layouts/photos";
 import Videos from "./layouts/video";
+import useClientStore from "@/store/useClientStore";
+import useCoverStore from "@/store/useCoverStore";
+import { useMemo } from "react";
 
 interface Props {
   untuk: string;
@@ -23,26 +26,46 @@ const Luma: NextPage<Props> = ({ untuk }) => {
   const {
     state: { images },
   } = usePhotos();
+  const { client } = useClientStore();
+  const { isOpen } = useCoverStore();
 
-  const settings: Settings = {
-    dots: false,
-    fade: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    waitForAnimate: false,
-    autoplay: true,
-    arrows: false,
-    autoplaySpeed: 3000,
-    speed: 1500,
-    cssEase: "ease-in-out",
-  };
-
+  const settings: Settings = useMemo(
+    () => ({
+      dots: false,
+      fade: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      waitForAnimate: false,
+      autoplay: isOpen,
+      arrows: false,
+      autoplaySpeed: 3000,
+      speed: 1500,
+      cssEase: "ease-in-out",
+    }),
+    [isOpen]
+  );
   return (
     <Layout>
       <main className="bg-luma-dark max-w-lg ml-auto relative">
         <div className="fixed max-w-lg ml-auto inset-0 z-10">
-          <Slider {...settings} className="w-full h-full relative">
+          <Slider
+            {...settings}
+            key={`slider-${isOpen}`}
+            className="w-full h-full relative"
+          >
+            {client?.cover && (
+              <Image
+                key={`Cover Slider`}
+                sizes="(max-width: 600px) 480px, (max-width: 1024px) 768px, (max-width: 1440px) 1280px, 1280px"
+                fill
+                quality={100}
+                alt={`Cover Slider`}
+                priority
+                className="object-cover transform translate-y-0 lg:translate-y-0 transition-transform shimmer-dark"
+                src={client.cover as string}
+              />
+            )}
             {images.map((image, index) => (
               <Image
                 key={`Main Slider ${index + 1}`}
