f4774d6a6cecdb2e3a9f13e3b118a029f2939e5c
Ari Wiradana <ariwiradana5@gmail.com>
2025-10-08 17:49:57 +0800

    modified:   constants/sosmed.ts

--- constants/sosmed.ts
+++ constants/sosmed.ts
@@ -6,7 +6,7 @@ interface SosmedURLs {
 }
 
 export const sosmedURLs: SosmedURLs = {
-  whatsapp: "https://wa.me/6287849662887",
+  whatsapp: "https://wa.me/6281247686727",
   email: "mailto:moment.invitations@gmail.com",
   instagram: "https://instagram.com/moment.invitations",
   youtube: "https://youtube.com/@moment.invitations",
