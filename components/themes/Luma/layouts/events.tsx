5598b20068496fa4bf2018650ee472936768cc71
Ari Wiradana <ariwiradana5@gmail.com>
2025-10-17 00:24:40 +0800

    modified:   components/themes/Luma/layouts/events.tsx

--- components/themes/Luma/layouts/events.tsx
+++ components/themes/Luma/layouts/events.tsx
@@ -45,21 +45,12 @@ const Events: NextPage = () => {
                 </p>
                 <div className="w-full h-[1px] bg-white/20 mb-2.5 hidden lg:block"></div>
                 <h2
-                  className="font-bigilla leading-[40px] lg:text-center relative -mb-1 lg:-mb-2 text-white text-[40px] md:text-5xl lg:text-7xl"
+                  className="font-bigilla leading-[40px] lg:text-center relative -mb-1.5 lg:-mb-2 text-white text-[40px] md:text-5xl lg:text-7xl"
                   aria-label={`Judul acara ${event.name}`}
                 >
                   {event?.name}
                 </h2>
               </div>
-
-              {/* {quotes[index] && (
-                <p
-                  className={`${rubik.className} mt-2 lg:mt-6 max-w-md mx-auto lg:text-center text-[10px] md:text-xs lg:text-sm font-light text-white`}
-                  aria-label={`Kutipan untuk acara ${event.name}`}
-                >
-                  {quotes[index]}
-                </p>
-              )} */}
             </div>
 
             <div
