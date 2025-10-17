0b587ca2411a918c83eaf0a636a48b715c0f9002
Ari Wiradana <ariwiradana5@gmail.com>
2025-10-17 23:35:53 +0800

    modified:   pages/[slug]/index.tsx

--- pages/[slug]/index.tsx
+++ pages/[slug]/index.tsx
@@ -49,8 +49,7 @@ export const getStaticProps: GetStaticProps<PageProps> = async (context) => {
   const { slug, untuk } = context.params as { slug: string; untuk: string };
 
   try {
-    const baseUrl =
-      process.env.API_BASE_URL || "https://momentinvitation.com/api";
+    const baseUrl = process.env.API_BASE_URL;
 
     const res = await getClient(`${baseUrl}/guest/seo?slug=${slug}`);
     if (!res.ok) throw new Error("Failed to fetch SEO data");
