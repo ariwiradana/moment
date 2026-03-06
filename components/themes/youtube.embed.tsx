97225629062bbce835fac0de0601c267eac9138b
Ari Wiradana <ariwiradana5@gmail.com>
2026-03-06 10:25:33 +0800

    modified:   components/themes/youtube.embed.tsx

--- components/themes/youtube.embed.tsx
+++ components/themes/youtube.embed.tsx
@@ -45,7 +45,7 @@ const YoutubeEmbed = ({ youtubeId, title }: YouTubeEmbedProps) => {
         title={title}
         className="absolute top-0 left-0 w-full h-full"
         frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
         allowFullScreen
         loading="lazy"
         onLoad={() => setVideoLoaded(true)}
